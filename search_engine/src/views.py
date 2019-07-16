adca717ed354626569951bc19fbf9545e630aa56
kabiroberai <oberai.kabir@gmail.com>
2019-07-16 20:25:49 +0530

    Begin working on landing page

--- search_engine/src/views.py
+++ search_engine/src/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'src/index.html')
+
+
+def search(request):
+    q: str = request.GET.get('q')
+    if q is None:
+        return redirect('index')
+    q = q.strip()
+    if q == "":
+        return redirect('index')
+    return HttpResponse(f"You searched for '<strong>{ request.GET.get('q') }</strong>'")
