26590c9fa0afde978ee8e936cd71e02674c671a2
kabiroberai <oberai.kabir@gmail.com>
2019-07-18 13:03:21 +0530

    Implement basic indexer and database structure

--- search_engine/src/management/commands/scraper/indexer.py
+++ search_engine/src/management/commands/scraper/indexer.py
@@ -1,4 +1,24 @@
+from src.models import Entry, SearchTerm
+from json import dumps
+
+
 def index(parsed, graph):
     print(" > ".join(graph))
-    print(parsed)
-    print()
+    entry = Entry(
+        url=graph[-1],
+        shouldFollow=parsed.should_follow,
+        title=parsed.title,
+        text=parsed.text,
+        open_graph=dumps(parsed.open_graph)
+    )
+    entry.save()
+    # at present we just store the raw word, but in a later task we'll strip punctuation etc
+    # and also store concepts
+    for word in parsed.word_set:
+        try:
+            term = SearchTerm.objects.get(pk=word)
+        except SearchTerm.DoesNotExist:
+            term = SearchTerm(term=word)
+            term.save()
+        term.entries.add(entry)
+
--- search_engine/src/models.py
+++ search_engine/src/models.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    url = models.TextField(primary_key=True)
+    shouldFollow = models.BooleanField()
+    title = models.TextField()
+    text = models.TextField()
+    open_graph = models.TextField()  # as json
+
+    def __str__(self):
+        return self.url
+
+
+# inverted index
+class SearchTerm(models.Model):
+    term = models.CharField(max_length=100, primary_key=True)
+    entries = models.ManyToManyField(Entry)
+
+    def __str__(self):
+        return self.term
